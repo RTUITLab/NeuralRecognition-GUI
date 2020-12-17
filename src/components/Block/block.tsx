6f911323055860cf05a9554785b841ce4d5f1e10
IvLaptev <ivlaptev13@ya.ru>
2020-12-17 23:50:28 +0300

    Rendered blocks

--- src/components/Block/block.tsx
+++ src/components/Block/block.tsx
@@ -1,50 +1,106 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 
-class Block extends React.Component<{}, BlockState> {
-    constructor(props: any) {
-        super(props);
+ export class Block {
+    public position: Coordinate;
+    private startPosition: Coordinate;
+    private fieldSize: Coordinate;
+    private size: number;
+
+    constructor(startPosition: Coordinate, fieldSize: Coordinate, size: number, private color: string) {
+        this.position = startPosition;
+        this.startPosition = startPosition;
+        this.fieldSize = fieldSize;
+        this.size = size;
     }
-    
-    componentWillMount() {
-        this.setState({window: { width: 0, height: 0 }})
-    }
 
-    componentDidMount() {
-
-        let window = document.getElementById('Field');
-
-        this.setState({
-            window: {
-                width: window?.offsetHeight,
-                height: window?.offsetWidth
-            }
+    public getStyle(origin: Coordinate): CSSProperties {        
+        return ({
+            position: 'absolute',
+            top: this.position.y + origin.y,
+            left: this.position.x + origin.x,
+            width: this.size,
+            height: this.size,
+            background: this.color,
+            borderRadius: 10
         })
     }
+}
 
+export class LargeBlock extends Block {
+    constructor(fieldSize: Coordinate) {
+        let blockSize = fieldSize.x / 6 > fieldSize.y / 7 ? fieldSize.y * 3 / 7 : fieldSize.x / 2;
+        const startPosition: Coordinate = {
+            x: fieldSize.x / 2 - 2 * blockSize / 3 + 5,
+            y: fieldSize.y / 2 - blockSize / 2 + 5
+        };
+        blockSize = blockSize - 10;
+        const blockField: Coordinate = {
+            x: fieldSize.x - blockSize,
+            y: fieldSize.y - blockSize
+        }
+
+        super(startPosition, blockField, blockSize, '#ffcc3c');
+    }
+}
+
+export class SmallBlock extends Block {
+    constructor(fieldSize: Coordinate, position: number) {
+        let blockSize = fieldSize.x / 6 > fieldSize.y / 7 ? fieldSize.y / 7 : fieldSize.x / 6;
+        
+        let startPosition: Coordinate = { x: 0, y: 0};
+        let color = '';
+        switch(position) {
+            case (1): {
+                startPosition = {
+                    x: fieldSize.x / 2 + blockSize + 5,
+                    y: fieldSize.y / 2 - 3 * blockSize / 2 + 5
+                };
+                color = '#ff2c22';
+                break;
+            }
+            case (2): {
+                startPosition = {
+                    x: fieldSize.x / 2 + blockSize + 5,
+                    y: fieldSize.y / 2 - blockSize / 2 + 5
+                };
+                color = '#00dc00';
+                break;
+            }
+            case (3): {
+                startPosition = {
+                    x: fieldSize.x / 2 + blockSize + 5,
+                    y: fieldSize.y / 2 + blockSize / 2 + 5
+                };
+                color = '#445bff';
+                break;
+            }
+        }
+        blockSize = blockSize - 10;
+
+        const blockField: Coordinate = {
+            x: fieldSize.x - blockSize,
+            y: fieldSize.y - blockSize
+        }
+
+        super(startPosition, blockField, blockSize, color);
+    }
+}
+
+export class VisualBlock extends React.Component<BlockProps, {}> {
     render() {
         return (
-            <div>{this.state.window.width}x{this.state.window.height}</div>
+            <div style={this.props.Style}></div>
         )
     }
 }
 
-type BlockProps = {
-    field: HTMLElement | null
+export type Coordinate = {
+    x: number,
+    y: number
 }
 
-type BlockState = {
-    window: {
-        width?: number,
-        height?: number
-    },
-    originPoint?: {
-        x: number,
-        y: number
-    },
-    startPosition?: {
-        x: number,
-        y: number
-    }
+type BlockProps = {
+    Style: React.CSSProperties
 }
 
 export default Block;
--- src/components/Field/field.tsx
+++ src/components/Field/field.tsx
@@ -1,35 +1,63 @@
 import React from 'react';
-import Block from '../Block/block';
+import { Block, LargeBlock, SmallBlock, Coordinate, VisualBlock } from '../Block/block';
 import './field.css';
 
-type FieldState = {
-    window: {
-        width?: number,
-        height?: number
-    }
-}
-
 class Field extends React.Component<{}, FieldState> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            window: {
-                width: 0,
-                height: 0
-            }        
+
+    componentWillMount() {
+        if (!this.state) {
+            this.setState({
+                blocks: []
+            });
         }
     }
 
     componentDidMount() {
+        const field = document.getElementById('Field')!;
+        
+        let origin: Coordinate = {
+            x: field.getBoundingClientRect().left,
+            y: field.getBoundingClientRect().top
+        }
+
+        let fieldSize: Coordinate = {
+            x: field.offsetWidth,
+            y: field.offsetHeight
+        }
+
+        let blocks: Array<Block> = [
+            new LargeBlock(fieldSize),
+            new SmallBlock(fieldSize, 1),
+            new SmallBlock(fieldSize, 2),
+            new SmallBlock(fieldSize, 3),
+        ]
+        this.setState({
+            blocks: blocks,
+            origin: origin
+        });
+
+        /*setInterval(() => {
+            //console.log("bzzzz");
+            let a = this.state.blocks;
+            a[0].position = { x: a[0].position.x - 1, y: 0};
+            this.setState({blocks: blocks});
+        }, 1);*/
     }
 
     render() {
         return (
             <div id="Field" className="Field">
-                <Block></Block>
+                {this.state.blocks.map((block, index) => (
+                    <VisualBlock key={index} Style={block.getStyle(this.state.origin)}>123</VisualBlock>
+                ))}
             </div>
         )
     }
+}
+
+type FieldState = {
+    blocks: Array<Block>,
+    origin: Coordinate
 }
 
 export default Field;
